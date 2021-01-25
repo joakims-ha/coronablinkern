9e7139a0a58eb589d1f348ee0d3b1c59fd27eb01
joxsa <joakim.salmen@gmail.com>
2021-01-25 20:49:02 +0200

    Add some basic  functions

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
+void init()
+{
+	// Create unique device id
+}
+
+int showMenu()
+{
+	// Display menu and get choice
+}
+
+void logDevice(int id)
+{
+	// Add device to list of contacts
+}
+
+void uploadLog()
+{
+	// Send log of devices to server
+}
+
 int main()
 {	
+	init();
 	printf("Hello World !!!\n");
 	return 0;
 }
+
