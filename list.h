ba0771a442bc5390bc69ab10ff6e57e5c90bf783
joxsa <joakim.salmen@gmail.com>
2021-02-02 14:55:27 +0200

    Add some doxygen comments.

--- list.h
+++ list.h
@@ -16,9 +16,34 @@ struct list_head {
 
 typedef struct list_head* list;
 
+/**
+ * Function for creating a new empty list.
+ * @returns a new empty list.
+ */
 list listCreate();
+
+/**
+ * Function for destroying a list.
+ * @param list the list to destroy.
+ * @returns nothing.
+ */
 void listDestroy(list l);
+
+/**
+ * Function for adding a new entry to a list
+ * @param list the list to add to.
+ * @param id id of new entry.
+ * @param date timestamp of new entry.
+ * @returns nothing.
+ */
 void listAdd(list l, int id, long int date);
+
+/**
+ * Function for removing old entries from a list
+ * @param list the list remove from.
+ * @param date timestamp of oldest entries to keep.
+ * @returns nothing.
+ */
 void listPrune(list l, long int date);
 
 #endif
