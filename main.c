a219dacad7f7487f7a707509fcdaf15eb8add31b
joxsa <joakim.salmen@gmail.com>
2021-03-03 14:59:28 +0200

    Add more menues for testing

--- main.c
+++ main.c
@@ -12,10 +12,25 @@ list_t devices;
 
 int sim_menu_list()
 {
+	time_t now;
+	time(&now);
+	int range = MAX_AGE*24*60*60*2;
 	for(int i = 1; i <= 10; i++)
     {
-		printf("Add random contact to list\n");
+		listAdd(devices, rand(), now-(rand()%range));
     }
+	return 1;
+}
+
+int list_menu_save()
+{
+	listSave(devices);
+	return 1;
+}
+
+int list_menu_show()
+{
+	uiShowList(devices);
 	return 1;
 }
 
@@ -38,12 +53,18 @@ int main()
 	devices = listCreate();
 	menu_t *main_menu = menuCreate(NULL,"Huvudmeny");
 	menu_t *sim_menu = menuCreate(main_menu,"Simulering");
+	menu_t *list_menu = menuCreate(sim_menu,"Lista");
 	
 	menuAddMenu(main_menu, "Simulering", sim_menu);
+	menuAddMenu(sim_menu, "Lista", list_menu);
 	menuAddCall(main_menu, "Rapportera fall", main_menu_case, NULL);
+
 	menuAddCall(sim_menu, "Simulera kontakt", simContact, devices);
 	menuAddCall(sim_menu, "Simulera larm", simAlert, devices);
-	menuAddCall(sim_menu, "Skapa lista", sim_menu_list, NULL);
+
+	menuAddCall(list_menu, "Generera lista", sim_menu_list, NULL);
+	menuAddCall(list_menu, "Spara lista", list_menu_save, NULL);
+	menuAddCall(list_menu, "Visa lista", list_menu_show, NULL);
 
 	printf("\n--== Välkommen till Coronablinkern ==--\n");
 	printf("\n            ## v0.1 ##\n");
