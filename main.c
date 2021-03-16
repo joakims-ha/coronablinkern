8e1584b993843beb80125034ca28efa0444ecb3f
joxsa <joakim.salmen@gmail.com>
2021-03-16 16:30:25 +0200

    Change menues

--- main.c
+++ main.c
@@ -13,36 +13,33 @@ int main()
 	contact_list contacts = listCreate();
 	
 	menu_t *main_menu = menuCreate(NULL,"Huvudmeny");
-	menu_t *sim_menu = menuCreate(main_menu,"Simulering");
-	menu_t *list_menu = menuCreate(sim_menu,"Lista");
-	
+	menu_t *test_menu = menuCreate(main_menu,"Test");
+	menu_t *sim_menu = menuCreate(test_menu,"Simulering");
+	menu_t *list_menu = menuCreate(test_menu,"Lista");
+
 	menuAddCall(main_menu, "Rapportera fall", actionsSimulateCase, contacts);
-	menuAddMenu(main_menu, sim_menu);
-	menuAddMenu(sim_menu, list_menu);
+	menuAddMenu(main_menu, test_menu);
 
 	menuAddCall(sim_menu, "Simulera kontakt", actionsSimulateContact, contacts);
 	menuAddCall(sim_menu, "Simulera larm", actionsSimulateAlert, contacts);
 
-	menuAddCall(list_menu, "Generera lista", actionsCreateList, contacts);
 	menuAddCall(list_menu, "Spara lista", actionsSaveList, contacts);
 	menuAddCall(list_menu, "Ladda lista", actionsLoadList, contacts);
 	menuAddCall(list_menu, "Rensa lista", actionsPruneList, contacts);
 	menuAddCall(list_menu, "Visa lista", actionsShowList, contacts);
 
+	menuAddCall(test_menu, "Generera kontakter", actionsCreateList, contacts);
+	menuAddMenu(test_menu, sim_menu);
+	menuAddMenu(test_menu, list_menu);
+	
 	printf("\n--== Välkommen till Coronablinkern ==--\n");
-	printf("\n            ## v0.1 ##\n");
-	printf("\n---------------------------------------\n");
 	
 	listLoad(contacts, FILE_NAME);
-	//time(&now);
-	//listPrune(contacts, now-MAX_AGE);
-
+	time(&now);
+	listPrune(contacts, now-MAX_AGE);
 	while(menuSelection(main_menu))
-	{
-		printf("\n---------------------------------------\n");
-	}
-	//time(&now);
-	//listPrune(contacts, now-MAX_AGE);
+	time(&now);
+	listPrune(contacts, now-MAX_AGE);
 	listSave(contacts, FILE_NAME);
 }
 
