23a819beec189f0e60cee8fe1f13b1840c0d2282
joxsa <joakim.salmen@gmail.com>
2021-03-17 21:47:39 +0200

    Add generated doxygen pdf

--- config.h
+++ config.h
@@ -2,9 +2,16 @@
 #ifndef CONFIG
 #define CONFIG
 
-#define DEVICE_ID 1234567890    /*!< Id of this device */
-#define DEVICE_CODE 123         /*!< Unclock code to repport this device */
-#define MAX_AGE 1814400         /*!< Max age of entries in the device list */
+#define DEVICE_ID 1234567890
+/*!< Id number of this device */
+
+#define DEVICE_CODE 123
+/*!< Unlock code for reporting infection */
+
+#define MAX_AGE 1814400
+/*!< Max age of entries in the device list */
+
 #define FILE_NAME "list.bin"
+/*!< Name of file to store contacts in */
 
 #endif
